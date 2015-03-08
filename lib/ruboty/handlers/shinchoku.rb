d842508cf607dfbbd2091591c2df24faa7093ada
izumin5210 <masayuki@izumin.info>
2015-03-08 13:33:42 +0900

    Add Ruboty::Handlers::Shinchoku

---
+++ lib/ruboty/handlers/shinchoku.rb
@@ -0,0 +1,17 @@
+module Ruboty
+  module Handlers
+    class Shinchoku < Base
+      env :TUMBLR_API_KEY, 'Tumblr API Key'
+
+      on(
+        /.*(進捗).*/,
+        name: 'shinchoku',
+        description: 'Get random image from shinchokudodesuka.tumblr.com'
+      )
+
+      def shinchoku(message)
+        Ruboty::Shinchoku::Actions::GetRandomImage.new(message).call
+      end
+    end
+  end
+end
